59a44b00b3541aab47b718c2a920ae14b502de5a
Christian Parker <christianparker512@gmail.com>
2021-08-16 14:23:20 -0500

    javascript

---
+++ js/main.js
@@ -0,0 +1,23 @@
+'use strict';
+(function (){
+  const second =1000, minute = second * 60, hour = minute *60, day = hour *24;
+
+  let myBirthday  = "August, 2021 16:00:00",
+    countown = new Date(myBirthday).getTime(),
+    x = setInterval(function(){
+      let nowTime = new Date().getTime(),
+        distanceToBirthday = countdown - nowTime;
+
+        document.getElementById('days').innerText = Math.floor(distanceToBirthday/(day));
+      document.getElementById('hours').innerText = Math.floor((distanceToBirthday%(day)) /(hour));
+      document.getElementById('hours').innerText = Math.floor((distanceToBirthday%(hour)) /(minute));
+      document.getElementById('hours').innerText = Math.floor((distanceToBirthday%(minute)) /(second));
+
+      console.log(distanceToBirthday);
+
+      if(distanceToBirthday <0) {
+        let msg = document.getElementById('days').innerText = "done";
+        clearInterval(x);
+      }
+    },0)
+}());
